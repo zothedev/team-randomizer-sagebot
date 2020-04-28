289fc33264b3579782eabf5e772c94ff1e4ba2e5
Joey Carusone <jcar@joeys-mbp.home>
2020-04-28 18:21:59 -0400

    removed responding for now (v0.1.2)

--- bot.py
+++ bot.py
@@ -12,36 +12,6 @@ async def on_ready():
     print('Ready')
     await client.change_presence(activity = discord.Game("use .gen to begin!"))
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hi'):
-        msg = 'hi {0.author.mention}'.format(message)
-        await client.send(message.channel, msg)
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hey'):
-        msg = 'hey {0.author.mention}'.format(message)
-        await client.send(message.channel, msg)
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hello'):
-        msg = 'hello {0.author.mention}'.format(message)
-        await client.send(message.channel, msg)
-
 @client.command(name="gen")
 async def VoiceChannel(ctx):
     VoiceChannel = ctx.author.voice.channel
